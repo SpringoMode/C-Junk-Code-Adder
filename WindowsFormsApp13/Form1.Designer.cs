4c986fb9eaad0dd2bd81e4e10711de4293435308
Spring <spring@5char.cc>
2020-03-31 17:39:43 -0400

    Upload
    found this on my pc idk how

---
+++ WindowsFormsApp13/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApp13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("wstring");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("string");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("void");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("int");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("long");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("char");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("func+junk");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("bool");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("float");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Types", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4,
+            treeNode5,
+            treeNode6,
+            treeNode7,
+            treeNode8,
+            treeNode9});
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numb = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            ((System.ComponentModel.ISupportInitialize)(this.numb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Type";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 40);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(783, 398);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numb
+            // 
+            this.numb.Location = new System.Drawing.Point(316, 13);
+            this.numb.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numb.Name = "numb";
+            this.numb.Size = new System.Drawing.Size(120, 20);
+            this.numb.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(442, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Amount";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(675, 40);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node1";
+            treeNode1.Text = "wstring";
+            treeNode2.Name = "Node4";
+            treeNode2.Text = "string";
+            treeNode3.Name = "Node5";
+            treeNode3.Text = "void";
+            treeNode4.Name = "Node6";
+            treeNode4.Text = "int";
+            treeNode5.Name = "Node7";
+            treeNode5.Text = "long";
+            treeNode6.Name = "Node8";
+            treeNode6.Text = "char";
+            treeNode7.Name = "Node9";
+            treeNode7.Text = "func+junk";
+            treeNode8.Name = "Node10";
+            treeNode8.Text = "bool";
+            treeNode9.Name = "Node11";
+            treeNode9.Text = "float";
+            treeNode10.Name = "Node0";
+            treeNode10.Text = "Types";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode10});
+            this.treeView1.Size = new System.Drawing.Size(121, 97);
+            this.treeView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numb);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ WindowsFormsApp13/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp13
+{
+    public partial class Form1 : Form
+    {
+        string watermark = "Springs Junk Gen C++";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "wstring" || textBox1.Text == "string" || textBox1.Text == "void" || textBox1.Text == "int" || textBox1.Text == "long" || textBox1.Text == "char" || textBox1.Text == "func+junk" || textBox1.Text == "bool" || textBox1.Text == "float")
+            {
+                Gen();
+            }
+            else if (textBox1.Text == "" || textBox1.Text == "Type")
+            {
+                MessageBox.Show("Empty Text", watermark);
+            }
+            else
+            {
+                MessageBox.Show("Not a valid option", watermark);
+            }
+
+        }
+        int counter;
+        const string q = "\"";
+        void Gen()
+        {
+            if (textBox1.Text == "string")
+            {
+                while (counter != numb.Value)
+                {
+                    Random random = new Random();
+                    int num1 = random.Next(1, 26);
+                    int num2 = random.Next(1000, 50000000);
+                    string FunctionName = RandomString(30);
+
+
+                    richTextBox1.Text += $"string {FunctionName} = {q}{num2 + FunctionName + FunctionName}{q};\n";
+                    counter++;
+                }
+
+            }
+            else if (textBox1.Text == "wstring")
+            {
+                while (counter != numb.Value)
+                {
+                    Random random = new Random();
+                    int num1 = random.Next(1, 26);
+                    int num2 = random.Next(1000, 50000000);
+                    string FunctionName = RandomString(30);
+
+
+                    richTextBox1.Text += $"wstring {FunctionName} = L{q}{FunctionName + FunctionName + FunctionName}{q};\n";
+                    counter++;
+                }
+            }
+            else if (textBox1.Text == "void")
+            {
+                while (counter != numb.Value)
+                {
+                    Random random = new Random();
+                    int num1 = random.Next(1, 26);
+                    int num2 = random.Next(1000, 50000000);
+                    string FunctionName = RandomString(30);
+
+
+                    richTextBox1.Text += ("void " + FunctionName + FunctionName + FunctionName +"() { }\n");
+                    counter++;
+                }
+            }
+            else if (textBox1.Text == "int")
+            {
+                while (counter != numb.Value)
+                {
+                    Random random = new Random();
+                    int num1 = random.Next(1, 26);
+                    int num2 = random.Next(1000, 50000000);
+                    string FunctionName = RandomString(30);
+
+
+                    richTextBox1.Text += ($"int {FunctionName} = {num2};\n");
+                    counter++;
+                }
+            }
+            else if (textBox1.Text == "long")
+            {
+                while (counter != numb.Value)
+                {
+                    Random random = new Random();
+                    int num1 = random.Next(1, 26);
+                    int num2 = random.Next(1000, 50000000);
+                    string FunctionName = RandomString(30);
+
+
+                    richTextBox1.Text += ($"long {FunctionName + num2 + FunctionName + num1} = {num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 };\n");
+                    counter++;
+                }
+            }
+            else if (textBox1.Text == "char")
+            {
+                while (counter != numb.Value)
+                {
+                    Random random = new Random();
+                    int num1 = random.Next(1, 26);
+                    int num2 = random.Next(1000, 50000000);
+                    string FunctionName = RandomString(30);
+
+
+                    richTextBox1.Text += $"const char* {FunctionName + FunctionName} = {q}{FunctionName + num2 + FunctionName + FunctionName}{q};\n";
+                    counter++;
+                }
+            }
+            else if (textBox1.Text == "bool")
+            {
+                while (counter != numb.Value)
+                {
+                    Random random = new Random();
+                    int num1 = random.Next(1, 26);
+                    int num2 = random.Next(1000, 50000000);
+                    int num4 = random.Next(5, 50);
+                    int num5 = random.Next(1, 2);
+                    string FunctionName = RandomString(30);
+
+                    if (num4 > 15)
+                    {
+                        richTextBox1.Text += $"bool {FunctionName + num1} = true;\n";
+                    }
+                    else if (num4 < 15)
+                    {
+                        richTextBox1.Text += $"bool {FunctionName + num1} = false;\n";
+                    }
+                    if(num5 != 1)
+                    {
+                        richTextBox1.Text += $"bool {FunctionName + FunctionName};\n";
+                    }
+                    else
+                    {
+                        richTextBox1.Text += $"bool {FunctionName};\n";
+                    }
+
+                    counter++;
+                }
+            }
+            else if (textBox1.Text == "func+junk")
+            {
+                while (counter != numb.Value)
+                {
+
+                }
+            }
+            else if  (textBox1.Text == "float")
+            {
+                while (counter != numb.Value)
+                {
+                    Random random = new Random();
+                    int num1 = random.Next(1, 26);
+                    int num2 = random.Next(1000, 50000000);
+                    string FunctionName = RandomString(30);
+
+
+                    richTextBox1.Text += $"float {FunctionName + FunctionName} = {num1 + num2 + num1 + num2}.f;\n";
+                    counter++;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error Occuerd", watermark);
+            }
+        }
+    }
+}
